3ee5fbba9284922d88f435dd0e13779dcf5fcbf5
jkaloya <jkaloya@indeed.com>
2021-03-25 01:55:18 -0700

    jyoti_kaloya: Take Home Assessment

--- cypress/integration/interview/interview-part-3.spec.js
+++ cypress/integration/interview/interview-part-3.spec.js
@@ -1,2 +1,39 @@
+/// <reference types="cypress" />
+
 // Here's a usable zipcode for your test: 35125
 
+import {v4 as uuid4} from 'uuid';
+
+describe('New Subscription', () => {
+    it('New Subscription', () => {
+        const email = uuid4() + '@gmail.com';
+        const zip = '35125';
+        cy.visit('www.freshly.com/subscriptions/new');
+        cy.get('[data-test=email-field]').type(email)
+        cy.get('[data-test=email-field]').should('have.value', email)
+        cy.get('[data-test=zip-field]').type(zip)
+        cy.get('[data-test=zip-field]').should('have.value',zip)
+        cy.get('form').contains('Continue').click()
+        cy.url().should('eq', 'https://www.freshly.com/join-now/plan?brand=core&zip=' + zip)
+        cy.get('h2').contains('12').parent().parent().parent().click()
+        cy.get('[data-test=earliest-delivery-date]').click()
+        cy.get('form').contains('Next').click()
+        cy.url().should('eq', 'https://www.freshly.com/join-now/meals?brand=core&delivery_dates=2021-03-31&plan_id=428&zip=35125')
+        //Didn't have time to parameterize the above URL above
+        cy.get('[data-test-meal-id=696807]').contains('Add').click()
+        cy.get('[data-test-meal-id=696943]').contains('Add').click()
+        cy.get('[data-test-meal-id=697398]').contains('Add').click()
+        cy.get('[data-test-meal-id=697195]').contains('Add').click()
+        cy.get('[data-test-meal-id=697245]').contains('Add').click()
+        cy.get('[data-test-meal-id=697282]').contains('Add').click()
+        cy.get('[data-test-meal-id=696808]').contains('Add').click()
+        cy.get('[data-test-meal-id=697338]').contains('Add').click()
+        cy.get('[data-test-meal-id=697387]').contains('Add').click()
+        cy.get('[data-test-meal-id=696916]').contains('Add').click()
+        cy.get('[data-test-meal-id=697153]').contains('Add').click()
+        cy.get('[data-test-meal-id=697271]').contains('Add').click()
+        cy.get('[data-test-type=cart__confirm-button]').click()
+        cy.get('form').contains('First Name').type('John')
+        cy.get('form').contains('Last Name').type('Doe')
+    })
+})
--- server.js
+++ server.js
@@ -2,15 +2,57 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let users = require('./data/users.json');
+let deliveries = require('./data/deliveries.json');
+let subscriptions = require('./data/subscriptions.json');
+
 app.get('/users', (req, res) => {
-  res.send('Users!')
+
+  let id = req.query.id;
+  let email = req.query.email;
+  let subscriptionId = req.query.subscriptionId;
+
+  let usersSorted = users.sort( function( a, b ) {
+    return a.email < b.email ? -1 : a.email > b.email ? 1 : 0;
+  });
+
+  if (id == null && email == null && subscriptionId == null){
+    res.send(usersSorted)
+  } else if (id != null) {
+    res.send(findId(users, id))
+  } else if (email != null) {
+    res.send(findEmail(users, email))
+  } else if (subscriptionId != null) {
+    res.send(findSubId(users, subscriptionId))
+  }
 })
+
 app.get('/subscriptions', (req, res) => {
-  res.send('Subscriptions!')
+
+  let id = req.query.id;
+  let type = req.query.type;
+
+  if(id != null){
+    res.send(userDetails({users, subscriptions, deliveries, id}));
+  } else if(type != null){
+    res.send(findSubsType(subscriptions, type))
+  } else{
+    res.send("No Subscriptions found, please enter Id or Type")
+  }
 })
+
 app.get('/deliveries', (req, res) => {
-  res.send('Deliveries!')
+
+  let id = req.query.id;
+  let subscriptionId = req.query.subscriptionId;
+
+  if(id != null){
+    res.send(userDetails({users, subscriptions, deliveries, id}));
+  } else if(subscriptionId != null) {
+    res.send(findDeliveriesSubId(deliveries, subscriptionId))
+  }
 })
+
 app.get('/', (req, res) => {
   res.send('Nothing here bud.')
 })
@@ -18,3 +60,71 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Interview app listening at http://localhost:${port}`)
 })
+
+function findId(data, idToLookFor) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].id == idToLookFor) {
+      return(data[i]);
+    }
+  }
+}
+
+function findEmail(data, emailToLookFor) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].email == emailToLookFor) {
+      return(data[i]);
+    }
+  }
+}
+
+function findSubId(data, subIdToLookFor) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].subscription_id == subIdToLookFor) {
+      return(data[i]);
+    }
+  }
+}
+
+function userDetails({users, subscriptions, deliveries, id}) {
+  let userDetails = [];
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].id == id) {
+      userDetails.push(users[i]);
+    }
+  }
+  for (var i = 0; i < subscriptions.length; i++) {
+    if (subscriptions[i].id == id) {
+      userDetails.push(subscriptions[i]);
+    }
+  }
+  for (var i = 0; i < deliveries.length; i++) {
+    if (deliveries[i].id == id) {
+      userDetails.push(deliveries[i]);
+    }
+  }
+  return userDetails;
+}
+
+function findSubsType(data, type) {
+  let subscriptionsWithType = [];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].type == type) {
+      subscriptionsWithType.push(data[i]);
+    }
+  }
+  return subscriptionsWithType;
+}
+
+function findDeliveriesSubId(data, subscriptionId) {
+  let deliveriesWithSubId = [];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].subscription_id == subscriptionId) {
+      deliveriesWithSubId.push(data[i]);
+    }
+  }
+  return deliveriesWithSubId;
+}
+
+/* I didn't have enough time to add error messages for when id, email or Sub Id does not exist or is incorrect since I
+ * don't usually develop. I would also put these base methods in a separate file.
+ */
