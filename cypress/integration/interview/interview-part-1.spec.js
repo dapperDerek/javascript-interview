4bda269a444775ab1261680fef6289a5c9afea6d
jbalandran <javierbalandran@gmail.com>
2021-04-16 23:23:40 -0700

    New Users API including spec file

--- cypress/integration/interview/interview-part-1.spec.js
+++ cypress/integration/interview/interview-part-1.spec.js
@@ -1,10 +1,39 @@
 describe('Interview', () => {
   //  ----- Users API -----  \\
   it('When /users api is called with no params then it returns all users ordered by email ascending', () => {
-    // Todo
+    cy.request('http://localhost:3000/users')
+      .then(res => {
+        let users = [...res.body]
+        users.sort((a,b) => (a.email > b.email) ? 1 : -1)
+
+        expect(res.status).to.equal(200)
+        expect(users).to.deep.equal(res.body)
+      })
   })
   it('When /users api is called with id, email, or subscription_id param then it returns user', () => {
-    // Todo
+    cy.request('http://localhost:3000/users?id=1')
+      .then(res => {
+        expect(res.status).to.equal(200)
+        expect(res.body).to.have.property('id', 1)
+        expect(res.body).to.have.property('email')
+        expect(res.body).to.have.property('subscription_id')
+      })
+
+    cy.request('http://localhost:3000/users?email=cguerra1@answers.com')
+      .then(res => {
+        expect(res.status).to.equal(200)
+        expect(res.body).to.have.property('id')
+        expect(res.body).to.have.property('email', 'cguerra1@answers.com')
+        expect(res.body).to.have.property('subscription_id')
+      })
+
+    cy.request('http://localhost:3000/users?subscription_id=3')
+      .then(res => {
+        expect(res.status).to.equal(200)
+        expect(res.body).to.have.property('id')
+        expect(res.body).to.have.property('email')
+        expect(res.body).to.have.property('subscription_id', 3)
+      })
   })
 
 
--- server.js
+++ server.js
@@ -2,8 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let users = require('./data/users.json')
+
 app.get('/users', (req, res) => {
-  res.send('Users!')
+  const user_id = req.query.id
+  const user_email = req.query.email
+  const user_subscription_id = req.query.subscription_id
+  let user;
+
+  if (Object.keys(req.query).length === 0) {
+    users.sort((a,b) => (a.email > b.email) ? 1 : -1)
+    res.status(200).send(users)
+  }
+  else {
+    if (user_id) {
+      user = users.find(element => element.id === parseInt(user_id))
+    } 
+    else if (user_email) {
+      user = users.find(element => element.email === user_email)
+    } 
+    else if (user_subscription_id) {
+      user = users.find(element => element.subscription_id === parseInt(user_subscription_id))
+    }
+  
+    if (user == null) {
+      res.status(404).send("Not Found")
+    }
+    else {
+      res.status(200).send(user)
+    }
+  }
 })
 app.get('/subscriptions', (req, res) => {
   res.send('Subscriptions!')
